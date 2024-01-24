8ef9206270308c50ef6f3f2af50dc64a0d218ca3
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2024-01-24 16:49:27 +0700

    update gitignore

---
+++ src/locale/vi.js
@@ -0,0 +1,54 @@
+export default {
+  'grapesjs-mjml': {
+    category: '',
+    panels: {
+      buttons: {
+        undo: 'Undo',
+        redo: 'Redo',
+        desktop: 'Desktop',
+        tablet: 'Tablet',
+        mobile: 'Mobile',
+        import: 'Import MJML',
+      },
+      import: {
+        title: 'Import MJML',
+        button: 'Import',
+        label: '',
+      },
+      export: {
+        title: 'Export MJML',
+      },
+    },
+    components: {
+      names: {
+        body: 'Body',
+        button: 'Button',
+        column: 'Cột',
+        oneColumn: '1 Cột',
+        twoColumn: '2 Cột',
+        threeColumn: '3 Cột',
+        fourColumn: '4 Cột',
+        divider: 'Phân cách',
+        group: 'Group',
+        hero: 'Hero',
+        image: 'Ảnh',
+        navBar: 'Navbar',
+        navLink: 'Navbar Link',
+        section: 'Section',
+        socialGroup: 'Group Social',
+        socialElement: 'Social Element',
+        spacer: 'Phân dòng',
+        text: 'Text',
+        wrapper: 'Wrapper',
+        raw: 'Raw',
+      },
+    },
+    sectors: {
+      names: {
+        Dimension: 'Kích thước',
+        Typography: "Chỉnh sửa chữ",
+        Decorations: "Trang trí"
+      }
+    }
+  },
+};
